4033b71a876d0a342804fdaeb467b7dce7ccd66f
camilapgn <camilapiga99@gmail.com>
2024-11-15 16:51:15 -0700

    holy grail added

---
+++ test.py
@@ -0,0 +1,52 @@
+# Data types
+# string, integer, boolean, float, etc
+#string
+"esto es una string"
+#integer 
+12
+#boolean
+[True, False]
+
+# float
+12.45
+
+# operators
+# == equal to
+# != not equal to
+# < less than
+# > greater than
+
+names = ["ronald", "camila", "Byron"]
+numbers = [2, 3 ,4,6]
+
+#agregar elemntos a lista .append()
+#quitar elementos .pop()
+# visitar cada elemento de una lista
+
+# for loop
+for name in names:
+    print(name)
+
+for number in numbers:
+    print(number * 2)
+
+
+# mientras algo sea verdad tu siguele
+
+# repetir un aopcion u numero de veces desconocido
+option = ("cual es tu opcion")
+
+# index numero que representa un poision en una secuencia
+option = input("what is your option: ")
+while option != 5:
+    print("What do you want?")
+
+
+# haz un programa que multiplique por 3 los numeros de una lista
+numb = [1,4,6,9]
+for n in numb:
+    print(n * 3)
+
+    while option !=5:
+    if option 1==1:
+       print(f{products}'has been added to the cart')
